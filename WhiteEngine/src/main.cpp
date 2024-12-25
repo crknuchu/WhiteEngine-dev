96e5e28d3abdef728193f7c086aeac64ef68a2c4
crknuchu <79843085+crknuchu@users.noreply.github.com>
2024-12-25 19:13:43 +0100

    implement simple triangle with vertex and fragment shader

--- WhiteEngine/src/main.cpp
+++ WhiteEngine/src/main.cpp
@@ -1,15 +1,8 @@
 #include "engine.h"
 
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
-#include <glad/glad.h>
-
 int main() {
 
 	Engine e;
-
 
 	e.StartWindow();
 	e.InitAll();
