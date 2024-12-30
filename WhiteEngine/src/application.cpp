ad2d11706e481eea77f6baea2d2de0ae90c96bb6
crknuchu <79843085+crknuchu@users.noreply.github.com>
2024-12-30 11:30:06 +0100

    implement model loading

--- WhiteEngine/src/application.cpp
+++ WhiteEngine/src/application.cpp
@@ -5,26 +5,6 @@
 
 #include <iostream>
 
-// Vertex shader source code
-const char* vertexShaderSource = R"(
-#version 330 core
-layout (location = 0) in vec3 aPos;
-
-void main() {
-    gl_Position = vec4(aPos, 1.0);
-}
-)";
-
-// Fragment shader source code
-const char* fragmentShaderSource = R"(
-#version 330 core
-out vec4 FragColor;
-
-void main() {
-    FragColor = vec4(0.5, 0.0, 0.5, 1.0); // Purple color
-}
-)";
-
 unsigned int VBO, VAO, shaderProgram;
 
 int Application::Init() {
@@ -42,45 +22,6 @@ int Application::Init() {
         return -1;
     }
 
-    // Build and compile the shader program
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
-    glCompileShader(vertexShader);
-
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
-    glCompileShader(fragmentShader);
-
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    // Set up vertex data and buffers
-    float vertices[] = {
-        // positions
-        0.0f,  0.5f, 0.0f,
-       -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f
-    };
-
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-
     return 0;
 }
 
@@ -96,14 +37,6 @@ int Application::CreateWindow() {
 }
 
 void Application::SwapBuffers() {
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    // Render the triangle
-    glUseProgram(shaderProgram);
-    glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-
     glfwSwapBuffers(window);
     glfwPollEvents();
 }
