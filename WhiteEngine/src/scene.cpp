c37962c85c263f682fca5ed924801715d0f4b792
crknuchu <79843085+crknuchu@users.noreply.github.com>
2025-01-06 16:58:10 +0100

    implement support for multiple gameobjects

--- WhiteEngine/src/scene.cpp
+++ WhiteEngine/src/scene.cpp
@@ -11,12 +11,35 @@
 #include "gameobject.h"
 #include "spdlog/spdlog.h"
 
+#include <cmath>
+
+#define PI 3.14159265
+
 Scene::Scene() {
-	setCamera(new Camera(glm::vec3(0.0f, 0.0f, 5.0f)));
+	setCamera(new Camera(glm::vec3(0.0f, 0.0f, 10.0f)));
 	model = new Model("res/models/brick_cube/brick_cube.obj");
 	shader = new Shader("res/shaders/vertex.vert", "res/shaders/fragment.frag");
-	gameobject = new GameObject(model, shader);
-	gameobject->setRotation(glm::vec3(0.0f, 35.0f, 45.0f)); //test rotation
+
+	unsigned num = 10;
+	unsigned radius = 5;
+	for (int i = 1; i <= num; ++i) {
+		// Calculate the angle for this point
+		double angle = 2 * PI * i / num;
+
+		// Calculate the x and y coordinates based on the angle
+		double x = 0 + radius * cos(angle);
+		double y = 0 + radius * sin(angle);
+
+		GameObject* gameobject = new GameObject(model, shader);
+		gameobject->setPosition(glm::vec3(x, y, 0.0f));
+		gameobject->setRotation(glm::vec3(0.0f,  x * 10.0f, y * 10.0f));
+		gameobject->setScale(glm::vec3(i * 0.1f));
+		addGameObject(gameobject);
+	}
+}
+
+void Scene::addGameObject(GameObject* gameobject) {
+	gameobjects.push_back(gameobject);
 }
 
 //Scene::Scene(Camera camera, Model model, Shader shader) {
@@ -42,8 +65,10 @@ void Scene::Draw() {
 	shader->setMat4("projection", camera->GetProjectionMatrix(800, 600)); //change 800, 600 to window width and height
 	shader->setMat4("view", camera->GetViewMatrix());
 
-	// render the loaded model
-	gameobject->draw();
+	// render gameobjects
+	for (auto& gameobject : gameobjects) {
+		gameobject->draw();
+	}
 }
 
 void Scene::Update() {
--- WhiteEngine/src/scene.h
+++ WhiteEngine/src/scene.h
@@ -1,19 +1,17 @@
 #pragma once
 
-//#include <vector>
+#include <vector>
+
 class Model;
 class Shader;
 class Camera;
 class GameObject;
 
-
 class Scene {
 private:
-	
-	// std::vector<GameObject*> gameobjects;
-	Model* model = nullptr;
+	std::vector<GameObject*> gameobjects;
+	Model* model = nullptr; //this need to be replaced with an asset manager
 	Shader* shader = nullptr;
-	GameObject* gameobject = nullptr;
 
 public:
 	Camera* camera = nullptr;
@@ -23,7 +21,7 @@ public:
 	Scene();
 	~Scene();
 
-	//void addGameObject(std::shared_ptr<GameObject> gameobject);
+	void addGameObject(GameObject* gameobject);
 	//void addLight(std::shared_ptr<Light> light);
 	void setCamera(Camera* camera);
 };
